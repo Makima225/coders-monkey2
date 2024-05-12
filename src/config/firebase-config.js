3284b4715c3cdd72de7d65284b9762d28cd8b992
Bauer_san <bauereudes225@gmail.com>
2024-05-12 00:29:57 +0000

    second commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAm7myRmnfAHokm3A9YA6QpbmwVk4gycl4",
+  authDomain: "coders-monkey-d3b1c.firebaseapp.com",
+  projectId: "coders-monkey-d3b1c",
+  storageBucket: "coders-monkey-d3b1c.appspot.com",
+  messagingSenderId: "149469176411",
+  appId: "1:149469176411:web:ab014f14b0169ba0cbd57e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
